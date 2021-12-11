251c9f2c6ffff1f79107dc3b9cba99918e6c8a26
Oluwatofunmi <olustrongman@icloud.com>
2021-12-10 20:48:21 -0500

    Complete qualified assignment, ...

---
+++ src/pomodoro/Break.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { minutesToDuration } from "../utils/duration";
+
+function Break({ breakDuration, breakMinus, breakPlus, session }) {
+
+  return (
+    <div className="col">
+      <div className="float-right">
+        <div className="input-group input-group-lg mb-2">
+          <span className="input-group-text" data-testid="duration-break">
+            Break Duration: {minutesToDuration(breakDuration)}
+          </span>
+          <div className="input-group-append">
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-testid="decrease-break"
+              onClick={breakMinus}
+              disabled={breakDuration === 1 || !!session}
+            >
+              <span className="oi oi-minus" />
+            </button>
+            <button
+              type="button"
+              className="btn btn-secondary"
+              data-testid="increase-break"
+              onClick={breakPlus}
+              disabled={breakDuration === 15 || !!session}
+            >
+              <span className="oi oi-plus" />
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Break;
